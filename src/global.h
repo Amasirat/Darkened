f09fb88f4ef5ed228387d92be4d7f0a4a874a8b7
Amasirat <amir.basirat.2004@gmail.com>
2023-08-18 00:44:57 +0330

    added global variables

---
+++ src/global.h
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+//namespace for default game related variables like default stat size
+//or default player levels, etc...
+namespace game
+{
+   inline const int default_stat_size{5}; 
+};
+//namespace for default system values like file
+//directories, configs etc...
+namespace system
+{
+
+};
+
+#endif
