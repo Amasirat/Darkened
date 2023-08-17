06d7f84d40ac9d335f2286184bc5a8bc82e1e019
Amasirat <amir.basirat.2004@gmail.com>
2023-08-17 21:22:53 +0330

    configured Cmake

---
+++ build/CMakeFiles/darkened.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/darkened.dir/src/main.cpp.o"
+  "CMakeFiles/darkened.dir/src/main.cpp.o.d"
+  "darkened"
+  "darkened.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/darkened.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
