5cc25d7f394e7d60d18a7db0330e13bfbd80f8dd
Amasirat <amir.basirat.2004@gmail.com>
2023-08-18 12:59:32 +0330

    tweaked run_test.py a bit more

--- run_test.py
+++ run_test.py
@@ -1,25 +1,22 @@
 import sys
 import os
-import subprocess
 """
 This script automatically runs test on one class and takes in only one argument for the directory of the .cpp file of the intended
 class of the function intended to be tested
 
 example: python run_test.py src/stat.cpp
 """
+# Global Variables
+TEST_EXEC_DIR = "../tests/test_runner.cpp"
+ERROR_CPP = "../src/error.cpp"
+TEST_EXEC = "test_run"
 # Compiles and runs test program taking the directory to the appropiate .cpp directory of the class
 # intending to be tested
-TEST_EXEC_CPP = "test_runner.cpp"
-ERROR_CPP = "../src/error.cpp"
-TEST_EXEC = "test"
 def run_test(test_directory):
-    clang_command = f"clang++ -std=c++17 {ERROR_CPP} {TEST_EXEC_CPP} {test_directory} -o {TEST_EXEC}"
-    os.chdir("tests")
+    clang_command = f"clang++ -std=c++17 {ERROR_CPP} {TEST_EXEC_DIR} {test_directory} -o {TEST_EXEC}"
     result = os.system(clang_command)
     if result == 0:
         os.system(f"./{TEST_EXEC}")
-    else:
-        return
 
 # main function
 def main():
