f8c732970582ff1c94a3e77bdd9638bf0d0db190
Amasirat <amir.basirat.2004@gmail.com>
2023-08-18 13:06:49 +0330

    more testing tweaking

--- run_test.py
+++ run_test.py
@@ -4,12 +4,12 @@ import os
 This script automatically runs test on one class and takes in only one argument for the directory of the .cpp file of the intended
 class of the function intended to be tested
 
-example: python run_test.py ../src/stat.cpp
+example: python run_test.py src/stat.cpp
 """
 # Global Variables
-TEST_EXEC_DIR = "../tests/test_runner.cpp"
-ERROR_CPP = "../src/error.cpp"
-TEST_EXEC = "test_run"
+TEST_EXEC_DIR = "tests/test_runner.cpp"
+ERROR_CPP = "src/error.cpp"
+TEST_EXEC = "tests/test_run"
 # Compiles and runs test program taking the directory to the appropiate .cpp directory of the class
 # intending to be tested
 def run_test(test_directory):
