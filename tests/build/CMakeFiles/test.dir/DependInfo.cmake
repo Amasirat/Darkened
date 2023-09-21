ab148c384c6c21dd4927a81be13d81088680fffe
amasirat <amir.basirat.2004@gmail.com>
2023-09-21 22:04:35 +0330

    created terminalIO module

---
+++ tests/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/amasirat/projects/Darkened/tests/test_runner.cpp" "CMakeFiles/test.dir/test_runner.cpp.o" "gcc" "CMakeFiles/test.dir/test_runner.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -1,14 +1,7 @@
-#include "unit_tests.h"
 #include <iostream>
-#include <filesystem>
-#include <fstream>
-#include <ctime>
 
 int main()
 {
-    time_t now = time(0);
-
-    char* date_time = ctime(&now);
-    std::ofstream file(date_time);
+    std::cout << "Hello, World!\n";
     return 0;
 }
