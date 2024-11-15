dd053518ec4296704892c37d02b86cbeb5b5e318
amasirat <amir.basirat.2004@gmail.com>
2024-11-15 17:18:35 +0330

    added comments

--- Darkened/Main/Game.cs
+++ Darkened/Main/Game.cs
@@ -2,12 +2,13 @@
 using SFML.Graphics;
 
 namespace Darkened.Main;
+// This class contains the entry point into the program and is responsible for managing the window
 public static class Game
 {
     public static void Main()
     {
         Initialize();
-        
+        // Main Render Loop
         while (window.IsOpen)
         {
             window.Clear();
@@ -17,6 +18,7 @@ public static class Game
         }
     }
 
+    // Make all initialization code here
     private static void Initialize()
     {
         windowMode = new VideoMode(800, 600);
@@ -24,12 +26,14 @@ public static class Game
         window.Closed += OnClose;
     }
 
+    // Window calls this method whenever it wants to close, put anything that needs to be done before closing here
     private static void OnClose(object? sender, EventArgs e)
     {
         RenderWindow renderWindow = (RenderWindow)sender;
         renderWindow?.Close();
     }
     
+    // private fields
     private static string gameTitle = "Darkened";
     private static RenderWindow window;
     private static VideoMode windowMode;
