cf461034c5405c4318febda1d855e7bdfb7df683
amasirat <amir.basirat.2004@gmail.com>
2024-12-18 12:43:54 +0330

    tweaked the formatting of code

--- Darkened/Data/Globals.cs
+++ Darkened/Data/Globals.cs
@@ -4,8 +4,12 @@ public static class Globals
 {
     public static bool IsDebug { get; set; } = false;
     public const string ProjectName = "Darkened";
-    public static readonly string LogDirectory = !IsDebug ? Path.Join(
-        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), 
-        Path.Join(ProjectName, "Logs")) : Path.Join(Directory.
-        GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName, "obj", "Debug", "Logs");
+    public static readonly string LogDirectory = !IsDebug ? 
+        Path.Join(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), 
+            Path.Join(ProjectName, "Logs")) : 
+        
+        Path.Join(
+            Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName, 
+            "obj", "Debug", "Logs");
 }
