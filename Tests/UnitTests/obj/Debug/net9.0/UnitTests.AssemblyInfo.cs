85d2b6c59818da31997d9cabd692a32e4d325c73
amasirat <amir.basirat.2004@gmail.com>
2025-03-24 17:56:17 +0330

    fixed font not loading error

--- Tests/UnitTests/obj/Debug/net9.0/UnitTests.AssemblyInfo.cs
+++ Tests/UnitTests/obj/Debug/net9.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc30d1641a26d840aba8594e869e7e7139ed993f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+155f7847bf1f10feb8e7ab5e7729b48a9e938a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
